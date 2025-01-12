9677482edc83910cc8e5015242795313a37ed601
tuvllms <151902656+tuvllms@users.noreply.github.com>
2025-01-12 20:56:46 +0000

    Deploying to gh-pages from @ tuvllms/nlp-spring-2025@3d7a7df330b34b6e9ddf0d73dd7c8036d7864da9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/nlp-spring-2025/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/nlp-spring-2025/cv/";
-          },
-        },{id: "post-a-post-with-image-galleries",
+  },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
