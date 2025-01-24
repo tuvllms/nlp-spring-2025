147898297618d7f4d71a252ccb7702e2283faf5c
tuvllms <151902656+tuvllms@users.noreply.github.com>
2025-01-24 04:36:09 +0000

    Deploying to gh-pages from @ tuvllms/nlp-spring-2025@ae7e439421823d4c2d90611b38c7e46e5d6b0d02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,11 +55,11 @@ ninja.data = [{
           window.location.href = "/nlp-spring-2025/blog/2024/photo-gallery/";
         
       },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+    },{id: "post-500-server-error",
       
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: '500 Server Error <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
+      description: "",
       section: "Posts",
       handler: () => {
         
@@ -398,6 +398,9 @@ ninja.data = [{
       },
     },{id: "news-course-begins",
           title: 'Course begins!',
+          description: "",
+          section: "News",},{id: "news-homework-0-released",
+          title: 'Homework 0 released! 📚 🎉 ✨ 📝',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
